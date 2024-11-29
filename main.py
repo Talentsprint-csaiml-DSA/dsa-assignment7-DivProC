f3507f04776b9d2ab1599d344a4706f78ec50555
DivProC <divij712@gmail.com>
2024-11-29 11:51:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-def longest_common_subsequence():
-    # ToDo 
+def longest_common_subsequence(a, b):
+    Pos = [[0 for x in range(len(b) + 1)] for y in range(len(a)+1)]
+    for i in range(1, len(a) + 1):
+        for j in range(1, len(b) + 1):
+            if a[i-1] == b[j-1]:
+                Pos[i][j] = 1 + Pos[i-1][j-1]
+            else:
+                Pos[i][j] = max(Pos[i-1][j], Pos[i][j-1])
+    return Pos[i][j]
+               
